cf649bde28e0c9bf051c3c4b802bf47f76369162
Азамат <azamat.kydyraliev@gmail.com>
2024-12-02 00:13:09 +0600

    new repository

---
+++ accounts/src/main/java/com/eazybytes/accounts/dto/ResponseDto.java
@@ -0,0 +1,16 @@
+package com.eazybytes.accounts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDto {
+
+    private String statusCode;
+    private String statusMessage;
+}
